2d7a0c915b81f4bdc88aa1e8627414d4c58f892d
ShaoKan <tom19991119@gmail.com>
2019-08-31 02:53:51 -0400

    fixed an issue where the windows does not close off properly

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -14,23 +14,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     calendar.on("eventClick", (info) => {
         info.jsEvent.preventDefault();
-        if ($(window).width <= 768) {
-            if ($("#event-detail").length != 0) {
-                info.el.style.borderColor = "transparent";
-                $("#event-detail").remove();
-                return;
-            }
-        }
     })
 
     calendar.on("eventMouseEnter", (info) => {
-        info.el.style.borderColor = 'salmon';
         let pop_up = "";
         if (!info.event.allDay) {
             const start_time = info.event.start.getHours() + ":" + (info.event.start.getMinutes() < 10 ? "0" + info.event.start.getMinutes() : info.event.start.getMinutes());
             const end_time = info.event.end.getHours() + ":" + (info.event.end.getMinutes() < 10 ? "0" + info.event.end.getMinutes() : info.event.end.getMinutes());
+            const offset = $(info.el).offset();
             pop_up = `
-            <div id="event-detail">
+            <div id="event-detail" style="top: ${offset.top}px; left: ${offset.left}px">
                 <p>Details</p>
                 <p>start time: ${start_time}</p>
                 <p>end time: ${end_time}</p>
@@ -40,23 +33,22 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>`;
         }
         else {
+            const offset = $(info.el).offset();
             pop_up = `
-            <div id="event-detail">
+            <div id="event-detail" style="top: ${offset.top}px; left: ${offset.left}px">
                 <p>Details</p>
                 <div class="text-center">
                 <a class="event-link" href="${info.event.url}">links</a>
                 </div>
             </div>`;
         }
-        $("#event-detail a").on("click", () => {
-            window.open(info.event.url);
-        })
-        $(pop_up).appendTo(info.el);
-    })
-
-    calendar.on("eventMouseLeave", (info) => {
-        info.el.style.borderColor = "transparent";
-        $("#event-detail").remove();
+        let target = $(":root");
+        $(pop_up).appendTo(target);
+        $("#event-detail").on("mouseleave", remove_event);
     })
     calendar.render();
 });
+
+const remove_event = () => {
+    $("#event-detail").remove();
+}
