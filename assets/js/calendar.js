3d9a641c569bf5bb8b5260f555605f4263c34926
ShaoKan <tom19991119@gmail.com>
2019-08-31 01:59:07 -0400

    optimize mobile experience

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -12,42 +12,16 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // calendar.on("eventClick", (info) => {
-    //     info.jsEvent.preventDefault();
-    //     if ($("#event-detail").length != 0) {
-    //         info.el.style.borderColor = "transparent";
-    //         $("#event-detail").remove();
-    //         return;
-    //     }
-    //     info.el.style.borderColor = 'salmon';
-    //     let pop_up = "";
-    //     if (!info.event.allDay) {
-    //         const start_time = info.event.start.getHours() + ":" + (info.event.start.getMinutes() < 10 ? "0" + info.event.start.getMinutes() : info.event.start.getMinutes());
-    //         const end_time = info.event.end.getHours() + ":" + (info.event.end.getMinutes() < 10 ? "0" + info.event.end.getMinutes() : info.event.end.getMinutes());
-    //         pop_up = `
-    //         <div id="event-detail">
-    //             <p>Details</p>
-    //             <p>start time: ${start_time}</p>
-    //             <p>end time: ${end_time}</p>
-    //             <div class="text-center">
-    //             <a class="event-link" href="${info.event.url}">links</a>
-    //             </div>
-    //         </div>`;
-    //     }
-    //     else {
-    //         pop_up = `
-    //         <div id="event-detail">
-    //             <p>Details</p>
-    //             <div class="text-center">
-    //             <a class="event-link">links</a>
-    //             </div>
-    //         </div>`;
-    //     }
-    //     $("#event-detail a").on("click", () => {
-    //         window.open("info.event.url")
-    //     })
-    //     $(pop_up).appendTo(info.el);   
-    // })
+    calendar.on("eventClick", (info) => {
+        info.jsEvent.preventDefault();
+        if ($(window).width <= 768) {
+            if ($("#event-detail").length != 0) {
+                info.el.style.borderColor = "transparent";
+                $("#event-detail").remove();
+                return;
+            }
+        }
+    })
 
     calendar.on("eventMouseEnter", (info) => {
         info.el.style.borderColor = 'salmon';
@@ -75,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>`;
         }
         $("#event-detail a").on("click", () => {
-            window.open("info.event.url")
+            window.open(info.event.url);
         })
         $(pop_up).appendTo(info.el);
     })
