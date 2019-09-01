4c7b4f30109c03a23e904a62fcf19028f36a76dd
ShaoKan <tom19991119@gmail.com>
2019-09-01 14:14:08 -0400

    removed longpressdelay

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -46,32 +46,5 @@ document.addEventListener('DOMContentLoaded', function () {
     calendar.on("eventMouseLeave", (info) => {
         $("#event-detail").remove();
     })
-
-    calendar.on("eventLongPressDelay", (info) => {
-        let pop_up = "";
-        if (!info.event.allDay) {
-            const start_time = info.event.start.getHours() + ":" + (info.event.start.getMinutes() < 10 ? "0" + info.event.start.getMinutes() : info.event.start.getMinutes());
-            const end_time = info.event.end.getHours() + ":" + (info.event.end.getMinutes() < 10 ? "0" + info.event.end.getMinutes() : info.event.end.getMinutes());
-            pop_up = `
-            <div id="event-detail">
-                <p>Details</p>
-                <p>start time: ${start_time}</p>
-                <p>end time: ${end_time}</p>
-                <div class="text-center">
-                <p class="event-link">Click for details</p>
-                </div>
-            </div>`;
-        }
-        else {
-            pop_up = `
-            <div id="event-detail">
-                <p>Details</p>
-                <div class="text-center">
-                <p class="event-link">Click for details</p>
-                </div>
-            </div>`;
-        }
-        $(pop_up).appendTo(info.el);
-    })
     calendar.render();
 });
