9209272084e4018ba414f2a1101327b58340c2c9
ShaoKan <tom19991119@gmail.com>
2019-09-01 16:49:29 -0400

    finalize the design of calendar

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,7 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
     let calendarEl = document.getElementById('calendar');
 
     let calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: ['dayGrid']
+        plugins: ['dayGrid', 'timeGrid'],
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }
     });
 
     let event = $.getJSON("/assets/data/events.json", () => {
@@ -14,37 +19,57 @@ document.addEventListener('DOMContentLoaded', function () {
 
     calendar.on("eventClick", (info) => {
         info.jsEvent.preventDefault();
+        generate_popup(info);
     })
 
     calendar.on("eventMouseEnter", (info) => {
-        let pop_up = "";
-        if (!info.event.allDay) {
-            const start_time = info.event.start.getHours() + ":" + (info.event.start.getMinutes() < 10 ? "0" + info.event.start.getMinutes() : info.event.start.getMinutes());
-            const end_time = info.event.end.getHours() + ":" + (info.event.end.getMinutes() < 10 ? "0" + info.event.end.getMinutes() : info.event.end.getMinutes());
-            pop_up = `
-            <div id="event-detail">
-                <p>Details</p>
-                <p>start time: ${start_time}</p>
-                <p>end time: ${end_time}</p>
-                <div class="text-center">
-                <p class="event-link">Click for details</p>
-                </div>
-            </div>`;
-        }
-        else {
-            pop_up = `
-            <div id="event-detail">
-                <p>Details</p>
-                <div class="text-center">
-                <p class="event-link">Click for details</p>
-                </div>
-            </div>`;
-        }
-        $(pop_up).appendTo(info.el);
     })
 
     calendar.on("eventMouseLeave", (info) => {
-        $("#event-detail").remove();
+        //$("#event-detail").remove();
     })
+
     calendar.render();
 });
+
+const generate_popup = (info) => {
+    let pop_up = "";
+    if (!info.event.allDay) {
+        const start_time = info.event.start.getHours() + ":" + (info.event.start.getMinutes() < 10 ? "0" + info.event.start.getMinutes() : info.event.start.getMinutes());
+        const end_time = info.event.end.getHours() + ":" + (info.event.end.getMinutes() < 10 ? "0" + info.event.end.getMinutes() : info.event.end.getMinutes());
+        let x = $(info.el).parent().position();
+        let test = $(info.el).parents(".fc-row").position();
+        pop_up = `
+        <div id="event-detail" style="top: ${x.top + test.top + 24 - 10}px; left: ${x.left - 15}px">
+            <div id="mdiv" class="mt-1 mr-1">
+                <div class="mdiv">
+                    <div class="md"></div>
+                </div>
+            </div>
+            <p>Details</p>
+            <p>start time: ${start_time}</p>
+            <p>end time: ${end_time}</p>
+        </div>`;
+    }
+    else {
+        let x = $(info.el).parent().position();
+        let test = $(info.el).parents(".fc-row").position();
+        pop_up = `
+        <div id="event-detail" style="top: ${x.top + test.top + 24 - 10}px; left: ${x.left - 15}px">
+            <div id="mdiv" class="mt-1 mr-1">
+                <div class="mdiv">
+                    <div class="md"></div>
+                </div>
+            </div>
+            <p>Details</p>
+            <p>time: All Day</p>
+        </div>`;
+    }
+    const target = $(info.el).parents(".fc-view-container");
+    $(pop_up).appendTo(target);
+    $("#event-detail #mdiv").on("click", (e) => {
+        console.log("click");
+        console.log(e);
+        $(e.target).parents("#event-detail").remove();
+    });
+}
