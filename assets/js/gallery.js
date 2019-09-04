16df595bf20e27458ac60b1179e233abbd09ff87
ShaoKan <tom19991119@gmail.com>
2019-09-04 00:58:08 -0400

    added modal image functionality

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -10,3 +10,23 @@ $(function () {
 		}, 300);
 	});
 });
+
+$("#gallery img").on("click", (e)=> {
+	const header = document.querySelector(".header-area");
+	header.style.display = "none";
+	const modal = document.getElementById("myModal");
+	console.log(e);
+	const target_img = e.target;
+	const modalImg = document.getElementById("img01");
+	const captionText = document.getElementById("caption");
+	modal.style.display = "block";
+	modalImg.src = target_img.src;
+	captionText.innerHTML = target_img.alt;
+})
+
+$(".close").on("click", ()=> {
+	const modal = document.getElementById("myModal");
+	modal.style.display = "none";
+	const header = document.querySelector(".header-area");
+	header.style.display = "block";
+})
